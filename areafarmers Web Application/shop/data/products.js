9bec03659f94a9aeb2bbb4534fe99647823accc2
popmonac <lawsonthompson57@gmail.com>
2023-11-15 14:31:36 +0100

    Buidl A Bull

---
+++ areafarmers Web Application/shop/data/products.js
@@ -0,0 +1,376 @@
+export const products = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/image2.jpg",
+    name: "Cartons of vegetables",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "potato",
+      "cartons",
+      "vegetables"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/products/image3.jpg",
+    name: "Bag of beans",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "beans",
+      "bags"
+    ]
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/egg10.jpg",
+    name: "Crates of eggs",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "eggs",
+      "crates",
+      "fowl"
+    ],
+    type: "eggs",
+    sizeChartLink: "images/products/egg10.jpg"
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: "images/products/image5.jpg",
+    name: "Basket of tubers",
+    rating: {
+      stars: 5,
+      count: 2197
+    },
+    priceCents: 1899,
+    keywords: [
+      "tubers",
+      "basket",
+      "food"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "images/products/image6.jpg",
+    name: "Plastic of Tomatoes",
+    rating: {
+      stars: 4,
+      count: 37
+    },
+    priceCents: 2067,
+    keywords: [
+      "plates",
+      "kitchen",
+      "dining"
+    ]
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "images/products/image3.jpg",
+    name: "Bags of grains",
+    rating: {
+      stars: 4.5,
+      count: 175
+    },
+    priceCents: 3499,
+    keywords: [
+      "bags",
+      "grains"
+    ]
+  },
+  {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: "images/products/image10.jpg",
+    name: "Plastics of fruits",
+    rating: {
+      stars: 4.5,
+      count: 317
+    },
+    priceCents: 2400,
+    keywords: [
+      "fruits",
+      "food",
+      "plastics"
+    ]
+  },
+  {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: "images/products/image11.jpg",
+    name: "Crates of vegetables",
+    rating: {
+      stars: 4.5,
+      count: 144
+    },
+    priceCents: 3599,
+    keywords: [
+      "vegetables",
+      "crates",
+      "food",
+      "eatables",
+      "healthy"
+    ]
+  },
+  {
+    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image: "images/products/image12.jpg",
+    name: "A crate of egg",
+    rating: {
+      stars: 4.5,
+      count: 305
+    },
+    priceCents: 2899,
+    keywords: [
+      "egg",
+      "crate"
+    ]
+  },
+  {
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: "images/products/image13.jpg",
+    name: "A crate of carrot",
+    rating: {
+      stars: 4,
+      count: 89
+    },
+    priceCents: 3390,
+    keywords: [
+      "vegetables",
+      "crate",
+      "carrot"
+    ]
+  },
+  {
+    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    image: "images/products/chicken11.jpg",
+    name: "Full Chicken",
+    rating: {
+      stars: 4.5,
+      count: 235
+    },
+    priceCents: 2070,
+    keywords: [
+      "healthy",
+      "fresh",
+      "poultry",
+      "full",
+      "chicken"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/products/chicken11.jpg"
+  },
+  {
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: "images/products/image16.jpg",
+    name: "A bowl of Potatoes",
+    rating: {
+      stars: 4.5,
+      count: 30
+    },
+    priceCents: 1560,
+    keywords: [
+      "potatoes",
+      "bowl"
+    ]
+  },
+  {
+    id: "04701903-bc79-49c6-bc11-1af7e3651358",
+    image: "images/products/egg12.jpg",
+    name: "Pieces of eggs",
+    rating: {
+      stars: 4.5,
+      count: 562
+    },
+    priceCents: 2499,
+    keywords: [
+      "eggs",
+      "poultry",
+      "pieces",
+      "fowl",
+      "hen"
+    ]
+  },
+  {
+    id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    image: "images/products/meat12.jpg",
+    name: "Whole Meat",
+    rating: {
+      stars: 4.5,
+      count: 232
+    },
+    priceCents: 4599,
+    keywords: [
+      "whole",
+      "meat",
+      "healthy"
+    ]
+  },
+  {
+    id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+    image: "images/products/carrot15.jpg",
+    name: "Sack of Carrot",
+    rating: {
+      stars: 4,
+      count: 160
+    },
+    priceCents: 1699,
+    keywords: [
+      "carrots",
+      "sack",
+      "vegetables"
+    ]
+  },
+  {
+    id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+    image: "images/products/eja1.jpg",
+    name: "Smoked Fish",
+    rating: {
+      stars: 5,
+      count: 846
+    },
+    priceCents: 3074,
+    keywords: [
+      "healthy",
+      "fish",
+      "smoked"
+    ]
+  },
+  {
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: "images/products/coco1.jpg",
+    name: "tuber of Cocoyam",
+    rating: {
+      stars: 4,
+      count: 99
+    },
+    priceCents: 2374,
+    keywords: [
+      "food",
+      "tuber",
+      "yam",
+      "cocoyam",
+      "cocoa"
+    ]
+  },
+  {
+    id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    image: "images/products/eja2.jpg",
+    name: "smoky rounded fish",
+    rating: {
+      stars: 4,
+      count: 215
+    },
+    priceCents: 2200,
+    keywords: [
+      "fish",
+      "smoky",
+      "smoked",
+      "food"
+    ]
+  },
+  {
+    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    image: "images/products/mymeat.jpg",
+    name: "big pieces of meat",
+    rating: {
+      stars: 4.5,
+      count: 52
+    },
+    priceCents: 1799,
+    keywords: [
+      "meatpieces",
+      "big",
+      "meat"
+    ]
+  },
+  
+
+
+  {
+    id: "d37a651a-d501-483b-aae6-a9659b0757a0",
+    image: "images/products/eja3.jpg",
+    name: "Varietis of Smoked Fish",
+    rating: {
+      stars: 4,
+      count: 126
+    },
+    priceCents: 2899,
+    keywords: [
+      "fish",
+      "varieties",
+      "smoked"
+    ]
+  },
+  {
+    id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+    image: "images/products/potatoes20.jpg",
+    name: "Sack of Potatoes",
+    rating: {
+      stars: 4.5,
+      count: 1211
+    },
+    priceCents: 2250,
+    keywords: [
+      "sack",
+      "potatoes",
+      "tuber"
+    ]
+  },
+  {
+    id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+    image: "images/products/image32.jpg",
+    name: "One Basket of Onions",
+    rating: {
+      stars: 4.5,
+      count: 363
+    },
+    priceCents: 3099,
+    keywords: [
+      "onion",
+      "bulb"
+    ]
+  },
+  {
+    id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
+    image: "images/products/fish90.jpg",
+    name: "Fresh Fish",
+    rating: {
+      stars: 4.5,
+      count: 93
+    },
+    priceCents: 2110,
+    keywords: [
+      "fish",
+      "fresh",
+      "eat"
+    ]
+  },
+  {
+    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    image: "images/products/meat12.jpg",
+    name: "Medium Pieces of Meat",
+    rating: {
+      stars: 4,
+      count: 89
+    },
+    priceCents: 3390,
+    keywords: [
+      "meat",
+      "medium",
+      "pieces",
+      "food"
+    ]
+  },
+ 
+  
+];
